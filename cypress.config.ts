ece21af871b5394e690bcf0f0d7726a8a71c5a56
rajputanshu706@gmail.com <rajputanshu706@gmail.com>
2025-01-19 11:14:18 +0530

    Add Allure Report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from 'cypress'
+import { allureCypress } from "allure-cypress/reporter";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "./reports/allure-results",
+      });
       const file = config.env.configFile || 'stage'
       config.env = require(`./config/${file}.json`);
       config.baseUrl = config.env.baseUrl;
       return config;
     },
+    // reporter: 'mochawesome',
+    // reporterOptions: {
+    //   charts: true,
+    //   reportDir: './reports/',
+    //   reportFilename: 'mochawesome_report',
+    //   overwrite: true
+    // },
     retries: 1,
     defaultCommandTimeout: 5000,
   },
